411be3b0994223b405cbe4150817a342b6752291
tuiii67 <jrachelsonpun0suy@hotmail.com>
2024-05-11 06:15:11 +0700

    demo

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("hello world");
+function combine(n, k) {
+  const result = [];
+  backtrack(1, []);
+  return result;
+  function backtrack(start, current) {
+    if (current.length === k) {
+      result.push([...current]);
+      return;
+    }
+    for (let i = start; i <= n; i++) {
+      current.push(i);
+      backtrack(i + 1, current);
+      current.pop();
+    }
+  }
+}
